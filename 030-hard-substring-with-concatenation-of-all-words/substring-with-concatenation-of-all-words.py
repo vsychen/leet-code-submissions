b2254e5a969144eaa8ddcd6f64652cb1431a2a89
Victor Chen <5411154+vsychen@users.noreply.github.com>
2025-05-03 02:33:32 -0300

    Question 30

--- 030-hard-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
+++ 030-hard-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
@@ -1,22 +1,12 @@
 class Solution(object):
-    def combineWords(self, words):
-        if len(words) == 0 or len(words) == 1: return words
-        elif len(list(set(words))) == 1 and len(words) != 1: return ["".join(words)]
-        elif len(words) == 2: return [words[0]+words[1], words[1]+words[0]]
+    def tryString(self, s, words):
+        if len(s) == 0: return True
+        elif len(list(set(words))) == 1 and len(words) != 1: return ("".join(words)) == s
         else:
-            combinations = []
             for i in range(len(words)):
-                w = words[i]
-                combinations += [w+s for s in self.combineWords(words[:i]+words[i+1:])]
-                
-                print(i, w, words)
-            return list(set(combinations))
-
-    def find(self, s, w):
-        try:
-            return s.index(w)
-        except:
-            return -1
+                if s.startswith(words[i]):
+                    return self.tryString(s[len(words[i]):], words[:i]+words[i+1:])
+            return False
 
     def findSubstring(self, s, words):
         """
@@ -24,26 +14,24 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[int]
         """
-        word_set = self.combineWords(words)
-        indexes = []
-        print(word_set)
-        for w in word_set:
-            i = 0
-            while i < len(s):
-                aux = self.find(s[i:], w)
-                if aux != -1:
-                    aux += i
-                    if aux not in indexes: indexes.append(aux)
-                    i = aux + 1
-                else:
-                    i = len(s)
-        indexes.sort()
+        try:
+            any(s.index(w) for w in words)
 
-        return indexes
+            indexes = []
+            word_size = len("".join(words))
 
-        
+            if len(list(set(words))) == 1 and len(words) != 1:
+                word = "".join(words)
+                for i in range(len(s)-word_size+1):
+                    if s[i:i+word_size] == word:
+                        indexes.append(i)
+            else:
+                for i in range(len(s)-word_size+1):
+                    if any(s[i:].startswith(w) for w in words):
+                        if self.tryString(s[i:i+word_size], words):
+                            if i not in indexes: indexes.append(i)
+                indexes.sort()
 
-a = Solution()
-# print(a.findSubstring("fffffffffffffffffffffffffffffffff", ["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"]))
-# print(a.findSubstring("bcabbcaabbccacacbabccacaababcbb", ["c","b","a","c","a","a","a","b","c"]))
-print(a.findSubstring("pjzkrkevzztxductzzxmxsvwjkxpvukmfjywwetvfnujhweiybwvvsrfequzkhossmootkmyxgjgfordrpapjuunmqnxxdrqrfgkrsjqbszgiqlcfnrpjlcwdrvbumtotzylshdvccdmsqoadfrpsvnwpizlwszrtyclhgilklydbmfhuywotjmktnwrfvizvnmfvvqfiokkdprznnnjycttprkxpuykhmpchiksyucbmtabiqkisgbhxngmhezrrqvayfsxauampdpxtafniiwfvdufhtwajrbkxtjzqjnfocdhekumttuqwovfjrgulhekcpjszyynadxhnttgmnxkduqmmyhzfnjhducesctufqbumxbamalqudeibljgbspeotkgvddcwgxidaiqcvgwykhbysjzlzfbupkqunuqtraxrlptivshhbihtsigtpipguhbhctcvubnhqipncyxfjebdnjyetnlnvmuxhzsdahkrscewabejifmxombiamxvauuitoltyymsarqcuuoezcbqpdaprxmsrickwpgwpsoplhugbikbkotzrtqkscekkgwjycfnvwfgdzogjzjvpcvixnsqsxacfwndzvrwrycwxrcismdhqapoojegggkocyrdtkzmiekhxoppctytvphjynrhtcvxcobxbcjjivtfjiwmduhzjokkbctweqtigwfhzorjlkpuuliaipbtfldinyetoybvugevwvhhhweejogrghllsouipabfafcxnhukcbtmxzshoyyufjhzadhrelweszbfgwpkzlwxkogyogutscvuhcllphshivnoteztpxsaoaacgxyaztuixhunrowzljqfqrahosheukhahhbiaxqzfmmwcjxountkevsvpbzjnilwpoermxrtlfroqoclexxisrdhvfsindffslyekrzwzqkpeocilatftymodgztjgybtyheqgcpwogdcjlnlesefgvimwbxcbzvaibspdjnrpqtyeilkcspknyylbwndvkffmzuriilxagyerjptbgeqgebiaqnvdubrtxibhvakcyotkfonmseszhczapxdlauexehhaireihxsplgdgmxfvaevrbadbwjbdrkfbbjjkgcztkcbwagtcnrtqryuqixtzhaakjlurnumzyovawrcjiwabuwretmdamfkxrgqgcdgbrdbnugzecbgyxxdqmisaqcyjkqrntxqmdrczxbebemcblftxplafnyoxqimkhcykwamvdsxjezkpgdpvopddptdfbprjustquhlazkjfluxrzopqdstulybnqvyknrchbphcarknnhhovweaqawdyxsqsqahkepluypwrzjegqtdoxfgzdkydeoxvrfhxusrujnmjzqrrlxglcmkiykldbiasnhrjbjekystzilrwkzhontwmehrfsrzfaqrbbxncphbzuuxeteshyrveamjsfiaharkcqxefghgceeixkdgkuboupxnwhnfigpkwnqdvzlydpidcljmflbccarbiegsmweklwngvygbqpescpeichmfidgsjmkvkofvkuehsmkkbocgejoiqcnafvuokelwuqsgkyoekaroptuvekfvmtxtqshcwsztkrzwrpabqrrhnlerxjojemcxel", ["dhvf","sind","ffsl","yekr","zwzq","kpeo","cila","tfty","modg","ztjg","ybty","heqg","cpwo","gdcj","lnle","sefg","vimw","bxcb"]))
+            return indexes
+        except:
+            return []
