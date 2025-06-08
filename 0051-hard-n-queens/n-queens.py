d25aa5797b6ed25667679c4c3523daab1084d08f
Victor Chen <5411154+vsychen@users.noreply.github.com>
2025-06-08 18:09:15 -0300

    Questions 51-52

---
+++ 0051-hard-n-queens/n-queens.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    # TOPICS: ARRAY/BACKTRACKING
+    # The scenarios where there's n queens in a NxN board can be replaced by a equivalent combination. It works because there's only one queen in a row
+    # (in the combination, the integer will refer to the position of the queen), one queen in a column (there's will be no repetition of elements). The
+    # only case that need to be tested is the diagonals; the queens will be in a diagonally-invalid position if abs(x2-x1) = abs(y2-y1) where (x1,y1) 
+    # is the position of the first queen and (x2,y2) is the position of the second queen.
+    # First get all combinations for 1<=i<=n. Check if each combination is valid (each row will have only one queen; as there's no repetition, 
+    # there will be only one queen per column too; the combination is valid diagonally if abs(x2-x1) is different to y2-y1 for each pair of queens)
+    # With all valid combinations in hand, render the boards and return them.
+    def getCombinations(self, numbers):
+        if len(numbers) == 1: return [numbers]
+        r = []
+        for i in range(len(numbers)):
+            r.extend([[numbers[i]]+e for e in self.getCombinations(numbers[:i]+numbers[i+1:])])
+        return r
+    
+    def isValid(self, combination):
+        for i in range(len(combination)-1):
+            for j in range(i+1, len(combination)):
+                if abs(combination[i] - combination[j]) == j-i: return False
+        return True
+
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        if n == 1: return [["Q"]]
+        elif n == 2 or n == 3: return []
+
+        combinations = self.getCombinations([x+1 for x in range(n)])
+        validCombinations = [c for c in combinations if self.isValid(c)]
+
+        answer = []
+        row = ("."*(n-1))
+        for vc in validCombinations:
+            temp = []
+            for aux in vc:
+                substr = row[:aux-1]+"Q"+row[aux-1:]
+                temp.append(substr)
+            answer.append(temp)
+        return answer
